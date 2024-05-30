3ab22aec338f4233f3878767a331108084b96b64
deepak01112002 <deepak8574757609@gmail.com>
2024-05-30 10:32:50 +0530

    first react app

---
+++ day1/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import './style.css'
+import Deepak from "./Deepak"
+function App() {
+  
+  return (
+    <div >
+       <Deepak />
+    </div>
+  );
+}
+
+export default App;
---
+++ day1/src/Deepak.jsx
@@ -0,0 +1,13 @@
+
+
+
+function Deepak(){
+    return(
+       <div>
+           <h1 style={{"color" : "red"}}>Hello world</h1>
+            <img src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmVkJTIwc2hvZXN8ZW58MHx8MHx8fDA%3D" alt="" />
+       </div>
+    )
+}
+
+export default Deepak
