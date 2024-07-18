eefe68ea042f340f2349d3695a4e009e1892a4a6
deepak01112002 <deepak8574757609@gmail.com>
2024-07-18 10:30:46 +0530

    counter redux

---
+++ counter_redux/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
+function App() {
+  
+    const data = useSelector((deepak)=>deepak)
+    console.log(data)
+    const dispatch = useDispatch()
+
+  return (
+    <>
+      <h1>Balance :- {data} </h1>
+      <button onClick={()=>dispatch({type : "Withdraw"})}>Withdraw</button>
+      <button onClick={()=>dispatch({type : "Deposit"})}>Deposit</button>
+    </>
+  )
+}
+
+export default App
